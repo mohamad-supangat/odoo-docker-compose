3df539fd001431e6eae66648f431628b750ee796
Mohamad Supangat <hantamkoding@gmail.com>
2023-12-22 09:19:08 +0700

    add dump mysql

---
+++ bin/dump_plant_uml_table.sh
@@ -0,0 +1,2 @@
+planter "postgres://odoo:odoo@127.0.0.1/bk?sslmode=disable" -t marketing_campaign -t marketing_activity -t marketing_mail_template -t marketing_participant -t marketing_trace  -o campaign.uml
+
